737db3b05149bad8f57b746a32babc2bd464cf6f
Juan Placencia <juan.placencia.512@gmail.com>
2014-05-30 11:37:19 -0500

    Fix default deck creation

--- generators/root/project/template/deck.js
+++ generators/root/project/template/deck.js
@@ -1,13 +1,15 @@
 'use strict';
 
 var Deck = require('lib/deck');
+require('bespoke-keys');
 require('bespoke-loop');
 require('bespoke-scale');
 
-module.exports = Deck.create(document.body)
-.push('a')
-.push('b')
-.start({
+module.exports = Deck.create()
+.push(require('./slides/title.md'))
+.push(require('./slides/intro.md'))
+.start(document.body, {
+  keys: true,
   loop: true,
   scale: true
 });
